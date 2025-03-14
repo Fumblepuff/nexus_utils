a347cfb2b23df4c02233627e6bdde912b72f2a32
Fumblepuff <jhkalu@gmail.com>
2025-03-13 21:58:13 -0400

    feat: added all widget exports

--- lib/nexus_utils.dart
+++ lib/nexus_utils.dart
@@ -1,7 +1,19 @@
 library nexus_utils;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// Utils
+export 'src/utils/text_scale_box.dart';
+export 'src/utils/format.dart';
+export 'src/utils/window_header.dart';
+export 'src/utils/gradients.dart';
+export 'src/utils/background.dart';
+
+/// Buttons
+export 'src/buttons/hover_button.dart';
+export 'src/buttons/sidebar_button.dart';
+export 'src/buttons/custom_popup_menu.dart';
+export 'src/buttons/info_tooltip.dart';
+export 'src/buttons/custom_tooltip.dart';
+
+/// Cards
+export 'src/cards/card_flat.dart';
+export 'src/cards/card_white.dart';
