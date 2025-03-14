8ff0f9f9a1ab7554772240eec8f8ee4aacca1e71
Fumblepuff <jhkalu@gmail.com>
2025-03-13 21:34:35 -0400

    feat: created info tooltip widget

---
+++ lib/src/buttons/info_tooltip.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../utils/text_format.dart';
+
+class InfoTooltip extends StatelessWidget {
+  final String infoText;
+  const InfoTooltip({super.key, required this.infoText});
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.help,
+      child: Tooltip(
+        triggerMode: TooltipTriggerMode.tap,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.all(10),
+        showDuration: const Duration(seconds: 4),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        message: infoText,
+        textStyle: roboto.copyWith(
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+        child: const FaIcon(
+          FontAwesomeIcons.circleQuestion,
+          color: Colors.grey,
+          size: 20,
+        ),
+      ),
+    );
+  }
+}
