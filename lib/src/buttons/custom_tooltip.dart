9849d1285e637669acd3b96637d65d1db88074aa
Fumblepuff <jhkalu@gmail.com>
2025-03-13 21:28:07 -0400

    feat: created custom tooltip widget

---
+++ lib/src/buttons/custom_tooltip.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../utils/text_format.dart';
+
+class CustomTooltip extends StatelessWidget {
+  final String message;
+  final Widget child;
+  const CustomTooltip({super.key, required this.message, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: message,
+      waitDuration: const Duration(milliseconds: 500),
+      showDuration: const Duration(seconds: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade500,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      textStyle: roboto.copyWith(
+        color: Colors.white,
+        fontSize: 12,
+      ),
+      child: child,
+    );
+  }
+}
