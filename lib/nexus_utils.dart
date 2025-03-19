062c516f1554d1f3e1ac1d50f0bd2d73454ffecc
Fumblepuff <jhkalu@gmail.com>
2025-03-19 11:48:54 -0400

    chore: exported new widgets

--- lib/nexus_utils.dart
+++ lib/nexus_utils.dart
@@ -6,6 +6,7 @@ export 'src/utils/format.dart';
 export 'src/utils/window_header.dart';
 export 'src/utils/gradients.dart';
 export 'src/utils/background.dart';
+export 'src/utils/functions.dart';
 
 /// Buttons
 export 'src/buttons/hover_button.dart';
@@ -21,3 +22,7 @@ export 'src/cards/card_white.dart';
 /// Icons
 export 'src/icons/empty_icons.dart';
 export 'src/icons/icon_loading_shimmer.dart';
+
+/// Markdown
+export 'src/markdown/markdown_parser.dart';
+export 'src/markdown/markdown_editor.dart';
