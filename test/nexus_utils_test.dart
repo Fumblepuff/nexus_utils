4f07a19821e4013c96f2bb8eda6ae9cd038e009b
Fumblepuff <jhkalu@gmail.com>
2025-04-04 10:23:20 -0400

    test: tested encryption functions

--- test/nexus_utils_test.dart
+++ test/nexus_utils_test.dart
@@ -1,12 +1,14 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:nexus_utils/nexus_utils.dart';
+import 'package:nexus_utils/nexus_utils.dart';
 
 void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  // });
+  test('Encrypt then decrypt a string', () {
+    String key =
+        "b8cc8e559b5915ad4a61c0414f9017882d91d8c1d5d4179ff1f87ac14f209d20";
+    String text = "Hello, World!";
+    String encryptedText = encrypt(text, key);
+    String decryptedText = decrypt(encryptedText, key);
+    expect(decryptedText, text);
+  });
 }
