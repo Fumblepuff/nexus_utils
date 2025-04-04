b647ce345c4d8101f0f517ae2e4657ea9a153898
Fumblepuff <jhkalu@gmail.com>
2025-04-04 10:22:57 -0400

    feat: created encryption functions

--- lib/src/utils/functions.dart
+++ lib/src/utils/functions.dart
@@ -1,6 +1,7 @@
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:encrypt/encrypt.dart';
 
 Future<void> launchURL(String url) async {
   if (await canLaunchUrl(Uri.parse(url))) {
@@ -31,4 +32,23 @@ Future<http.Response> callApi(String url, Map<String, dynamic> body,
       body: jsonEncode(body),
     );
   }
+}
+
+String encrypt(String text, String key) {
+  final keyBytes = Key.fromUtf8(key.padRight(32, '0').substring(0, 32));
+  final iv = IV.fromLength(16);
+  final encrypter = Encrypter(AES(keyBytes));
+
+  final encrypted = encrypter.encrypt(text, iv: iv);
+  return '${encrypted.base64}:${iv.base64}';
+}
+
+String decrypt(String encryptedText, String key) {
+  final keyBytes = Key.fromUtf8(key.padRight(32, '0').substring(0, 32));
+  final parts = encryptedText.split(':');
+  final iv = IV.fromBase64(parts[1]);
+  final encrypter = Encrypter(AES(keyBytes));
+
+  final decrypted = encrypter.decrypt64(parts[0], iv: iv);
+  return decrypted;
 }
